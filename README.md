9a77af1d855c4cbb3a25c186bd303b721a25f70c
Char <black_dragon_917@gmail.com>
2013-01-31 20:23:53 -0700

    Added the CPSC585 directory, so that libraries are included in this project.

--- README.md
+++
@@ -1,6 +0,0 @@
-mPULSE
-======
-
-W13 CPSC 585 project
-
-my name is BLAHHHHHHH! XP
--- mPulse/TestProject/Object_OBJ.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <vector>
-#include "NxVec3.h"
-
-class Object_OBJ
-{
-	NxVec3 position;
-};
