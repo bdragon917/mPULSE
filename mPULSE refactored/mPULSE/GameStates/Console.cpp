ca11bcf194f2730cb16f92dd109202adba67c539
Char <black_dragon_917@gmail.com>
2013-02-08 17:44:34 -0700

    Improved console: The console stores up to 20 command which can be accessed by pushing up console now logs commands that are more than 20 commands old.

--- mPULSE refactored/mPULSE/GameStates/Console.cpp
+++ mPULSE refactored/mPULSE/GameStates/Console.cpp
@@ -3,7 +3,28 @@
 
 Console::Console()
 {
-    
+    logPointer = 0;
+}
+
+void Console::propragateMsg(std::string feedbackMsg)
+{
+  
+        consoleOut[4] = consoleOut[3];
+        consoleOut[3] = consoleOut[2];
+        consoleOut[2] = consoleOut[1];
+        consoleOut[1] = consoleOut[0];
+        consoleOut[0] = feedbackMsg;
+
+        if (consoleString != "")
+            log.push_back(consoleString);
+
+        log.push_back(feedbackMsg);
+
+    consoleString = "";
+
+    if (log.size() != 0)
+    {logPointer = log.size() - 1;}
+    appendExcessLog();
 }
 
 void Console::propragateMsg()
@@ -15,10 +36,85 @@ void Console::propragateMsg()
         consoleOut[1] = consoleOut[0];
         consoleOut[0] = consoleString;
 
+        log.push_back(consoleString);
+
     consoleString = "";
+
+    if (log.size() != 0)
+    {logPointer = log.size() - 1;}
+    appendExcessLog();
 }
 
 void Console::goUp()
 {
-        consoleString = consoleOut[0];
+    printf ("logSize:%i pointer:%i", log.size(), logPointer);
+    if (log.size() != 0)
+    {
+        if (!(log.at(logPointer).empty()))      //check to see if valid?
+            consoleString = log.at(logPointer);
+        
+        if ( (logPointer - 1) > 0 )
+        {logPointer = logPointer - 1;}
+        else
+        {logPointer = log.size() - 1;}
+    }
+}
+
+
+void Console::appendExcessLog()
+{
+    //if log is bigger than x, take half of it and dump to file
+
+    const int AMT = 5;  //number of elements to dump/truncate
+
+    if (log.size() > 20)
+    {
+
+        std::ofstream out;
+        out.open( "log.txt", std::ios_base::app );
+        if( !out )
+        {
+           printf( "Couldn't open file.\n");
+        }
+        else
+        {
+            for (int i=0; i < AMT; i++)
+            {
+                out << log.at(i) + char(10) + char(13);
+            }
+
+            
+            log.erase(log.begin(), log.begin()+AMT );
+        }
+        out.close();
+    }
+}
+
+void Console::appendLog()
+{
+    //if log is bigger than x, take half of it and dump to file
+
+    const int AMT = 5;  //number of elements to dump/truncate
+
+    if (log.size() > 20)
+    {
+
+        std::ofstream out;
+        out.open( "log.txt", std::ios_base::app );
+        if( !out )
+        {
+           printf( "Couldn't open file.\n");
+        }
+        else
+        {
+            if (log.size() != 0)
+            {
+                for (int i=0; i < log.size(); i++)              //Untested
+                {
+                    out << log.at(i) + char(10) + char(13);
+                }
+            }
+        }
+        out.close();
+    }
 }
--- mPULSE refactored/mPULSE/GameStates/PlayState.cpp
+++ mPULSE refactored/mPULSE/GameStates/PlayState.cpp
@@ -46,18 +46,16 @@ bool PlayState::handleKeyboardMouseEvents(SDL_Event &KeyboardMouseEvents)
 
                 //Num Commands
                 if (renderingEngine->aConsole.consoleString == "num cars")
-                {renderingEngine->aConsole.consoleString = "Number of elements in cars: " + renderingEngine->FloatToString(cars.size());}
+                {renderingEngine->aConsole.propragateMsg("Number of elements in cars: " + renderingEngine->FloatToString(cars.size()));}
 
                 if (renderingEngine->aConsole.consoleString == "num actors")
-                {renderingEngine->aConsole.consoleString = "Number of actors in scene: " + renderingEngine->FloatToString(physicsEngine->getScene()->getNbActors());}
+                {renderingEngine->aConsole.propragateMsg("Number of actors in scene: " + renderingEngine->FloatToString(physicsEngine->getScene()->getNbActors()));}
 
                 if (renderingEngine->aConsole.consoleString == "num debugphysic")
                 {
-                    renderingEngine->aConsole.consoleString = "Number of Lines in debug physic: " + renderingEngine->FloatToString(physicsEngine->getScene()->getDebugRenderable()->getNbLines());
-                    renderingEngine->aConsole.propragateMsg();
-                    renderingEngine->aConsole.consoleString = "Number of Points in debug physic: " + renderingEngine->FloatToString(physicsEngine->getScene()->getDebugRenderable()->getNbPoints());
-                    renderingEngine->aConsole.propragateMsg();
-                    renderingEngine->aConsole.consoleString = "Number of Triangles in debug physic: " + renderingEngine->FloatToString(physicsEngine->getScene()->getDebugRenderable()->getNbTriangles());
+                    renderingEngine->aConsole.propragateMsg("Number of Lines in debug physic: " + renderingEngine->FloatToString(physicsEngine->getScene()->getDebugRenderable()->getNbLines()));
+                    renderingEngine->aConsole.propragateMsg("Number of Points in debug physic: " + renderingEngine->FloatToString(physicsEngine->getScene()->getDebugRenderable()->getNbPoints()));
+                    renderingEngine->aConsole.propragateMsg("Number of Triangles in debug physic: " + renderingEngine->FloatToString(physicsEngine->getScene()->getDebugRenderable()->getNbTriangles()));
                 }
                 
                 //Get Commands
@@ -80,7 +78,7 @@ bool PlayState::handleKeyboardMouseEvents(SDL_Event &KeyboardMouseEvents)
                 //master Commands
                 if (renderingEngine->aConsole.consoleString == "quit")
                 {
-                    renderingEngine->aConsole.consoleString = "Trying to Quit...";
+                    renderingEngine->aConsole.propragateMsg("Trying to Quit...");
                     return false;
                 }
 
