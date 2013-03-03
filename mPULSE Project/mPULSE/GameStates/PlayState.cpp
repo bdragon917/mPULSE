01a2323598e01776589d764f287c683808632553
charles <blackdragon917@gmail.com>
2013-03-02 20:29:33 -0700

    Changes to waypoints

--- mPULSE Project/mPULSE/GameStates/PlayState.cpp
+++ mPULSE Project/mPULSE/GameStates/PlayState.cpp
@@ -22,7 +22,7 @@ PlayState::PlayState()
     RenderableComponent* pc2_rc = new RenderableComponent(1,3);
     player2Car->rc.push_back(pc2_rc);
     
-    int num_AI = 0;
+    int num_AI = 5;
 
     for (int a=0;a<num_AI;a++)
     {
