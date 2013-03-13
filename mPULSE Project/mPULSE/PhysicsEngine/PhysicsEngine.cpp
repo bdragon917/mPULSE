ae6efc853600b3fc185c9e40451ff7f4f94b6a90
Nolan <nolan_rh@hotmail.com>
2013-03-13 17:05:45 -0600

    Playing with way points

--- mPULSE Project/mPULSE/PhysicsEngine/PhysicsEngine.cpp
+++ mPULSE Project/mPULSE/PhysicsEngine/PhysicsEngine.cpp
@@ -269,20 +269,27 @@ void PhysicsEngine::createWaypoints(std::vector<Waypoint*>* wps)
         if(waypoints[i]->type == Waypoint::WAYPOINT)
         {
 		    NxVec3 position(waypoints[i]->pos);
+			float angle = acos(waypoints[i]->ori.dot(NxVec3(1,0,0)));
+
+			NxQuat q;
+            q.fromAngleAxis(angle*(180.0f/3.14f), NxVec3(0,1,0));
+			NxMat33 orient;
+            orient.fromQuat(q);
 
 		    //The actor has one shape, a box, 1m on a side
 		    NxBoxShapeDesc boxDesc;
-		    boxDesc.dimensions.set(25.0,25.0,25.0);
+		    boxDesc.dimensions.set(25.0,25.0,1.0);
 		    boxDesc.shapeFlags |= NX_TRIGGER_ENABLE;
 
 		    NxActorDesc actorDesc;
 		    actorDesc.shapes.pushBack(&boxDesc);
 		    actorDesc.globalPose.t = position;
     
-		    NxActor *actor = scene->createActor(actorDesc);		
+		    NxActor *actor = scene->createActor(actorDesc);	
             CustomData* cd = new CustomData(CustomData::WAYPOINT,-1,-1,waypoints[i]);
 
 		    actor->userData = cd; 
+			actor->setGlobalOrientation(orient);
         }
         else if(waypoints[i]->type == Waypoint::PICKUP_SPAWN)
         {
