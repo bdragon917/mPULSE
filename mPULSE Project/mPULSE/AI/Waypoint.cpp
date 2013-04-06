99a1c56d5ebe9f97a2b0abc453461ac896a4f252
Lee Ringham <leeringham@gmail.com>
2013-04-06 17:53:41 -0600

    Pickup icon now displayed

--- mPULSE Project/mPULSE/AI/Waypoint.cpp
+++ mPULSE Project/mPULSE/AI/Waypoint.cpp
@@ -12,6 +12,7 @@ Waypoint::Waypoint(std::vector<std::string>* info)
     pos.x   = NxReal(atof(info->at(1).data()));
     pos.y   = NxReal(atof(info->at(2).data()));
     pos.z   = NxReal(atof(info->at(3).data()));
+
     id      = atoi(info->at(4).data());
     nextId  = atoi(info->at(5) .data());
 }
